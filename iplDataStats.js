5fd34fcc4b4acfb5ae616ef78fdb8f16130c379c
Pradeep Singh Rawat <rawatpradeep08.2011@gmail.com>
2018-11-22 12:12:50 +0530

    First part-To find number of matches played per season completed

--- iplDataStats.js
+++ iplDataStats.js
@@ -1,4 +1,27 @@
-let csvToJson = require('convert-csv-to-json');
+var csvToJson = require('convert-csv-to-json');
 
-let data= csvToJson.fieldDelimiter(',') .getJsonFromCsv('matches.csv');
-console.log(data[0]);
+var data= csvToJson.fieldDelimiter(',') .getJsonFromCsv('matches.csv');
+var TotalMatches=[];
+console.log(data[0].season); 
+let obj={};
+let count=1;
+for(let i=0;i<data.length-1;i++){
+        if((data[i].season) === (data[i+1].season)){
+              count++;
+        }
+        else{
+            obj.year=data[i].season;
+            obj.matches=count;
+            TotalMatches.push(obj);
+            count=1;
+            obj = {};
+        }
+}
+function compare(a,b) {
+    if (a.year < b.year)
+      return -1;
+    if (a.year > b.year)
+      return 1;
+    return 0;
+  }
+console.log(TotalMatches.sort(compare));
