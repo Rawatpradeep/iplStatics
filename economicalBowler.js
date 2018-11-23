b8c98f73ca797f39aee694df54891acde5f8e1d5
Pradeep Singh Rawat <rawatpradeep08.2011@gmail.com>
2018-11-23 10:57:20 +0530

    Part-4 completed Returning economy of Bolwer from season 2015

--- economicalBowler.js
+++ economicalBowler.js
@@ -6,5 +6,58 @@ var MatchData = getDetail.getMatchData('2015');
       let detail = MatchData[i].split(',')
       MatchId.push(detail[0]);
   }
-console.log(MatchId)
+//console.log(MatchId)
 var DeleveriesData= getDetail.getDeliveriesData(MatchId[0],MatchId[MatchId.length-1]);
+//console.log(DeleveriesData[DeleveriesData.length-1]);
+
+let BowlerData=[];
+let BowlerObj={};
+let Ball=1;
+for(let i=0;i<DeleveriesData.length-1;i++){
+  let singleval=DeleveriesData[i].split(',');
+  if(singleval[8]==(DeleveriesData[i+1].split(',')[8])){
+    
+  if(BowlerObj.hasOwnProperty(singleval[8])){
+    BowlerObj[singleval[8]]= BowlerObj[singleval[8]]+parseInt(singleval[17])
+    BowlerObj[singleval[8]+"Ball"]++;
+  }
+  else{
+    BowlerObj[singleval[8]]=parseInt(singleval[17]);
+    BowlerObj[singleval[8]+"Ball"]=Ball;
+  }
+}
+else{
+  if(BowlerObj.hasOwnProperty(singleval[8])){
+    BowlerObj[singleval[8]]= BowlerObj[singleval[8]]+parseInt(singleval[17]);
+    BowlerObj[singleval[8]+"Ball"]++;
+  }
+  else{
+    BowlerObj[singleval[8]].Runs=parseInt(singleval[17]);
+    BowlerObj[singleval[8]+"Ball"]=Ball;
+  }
+}
+}
+//console.log(BowlerObj);
+ let Bowler ={};
+ for(let i in BowlerObj){
+   if(!(/Ball/).test(i)){
+     let over = parseInt(BowlerObj[i+"Ball"])/6;
+     //console.log(parseInt(BowlerObj[i])/over);
+     Bowler[i]=(parseInt(BowlerObj[i])/over);
+     
+   }
+   else{
+     continue;
+   }
+   }
+ 
+ var sortable = [];
+for (var Bowlers in Bowler) {
+    sortable.push([Bowlers, Bowler[Bowlers]]);
+}
+
+sortable.sort(function(a, b) {
+    return a[1] - b[1];
+});
+BowlerData.push(sortable)
+console.log(BowlerData);
