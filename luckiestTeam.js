1f83612714d70b86dd109263507e1ee4359096f0
Pradeep Singh Rawat <pradeep.rawat@mountblue.io>
2018-12-06 17:07:05 +0530

    all team number of toss won

---
+++ luckiestTeam.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var csv = require('fast-csv');
+var matchesStream = fs.createReadStream("matches.csv");
+var luckiestTeam={};
+
+csv
+.fromStream(matchesStream,{headers : true})
+.on("data",function(match){
+    if(luckiestTeam.hasOwnProperty(match['toss_winner'])){
+        luckiestTeam[match['toss_winner']]++;
+    }
+    else{
+        luckiestTeam[match['toss_winner']]=1;
+    }
+})
+.on("end",function(){
+      console.log(sorting(luckiestTeam));
+    
+})
+
+function sorting(obj){
+  var output=[]
+    for(let i in obj){
+        let temp=[];
+        temp[0]=i;
+        temp[1]=obj[i]
+      output.push(temp); 
+  }   
+    console.log(output.sort((a,b)=>{
+        return b[1]-a[1];
+    }))
+}
