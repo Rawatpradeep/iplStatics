be6234678d09d815f31477263fdd1636f9092994
Pradeep Singh Rawat <pradeep.rawat@mountblue.io>
2018-12-06 17:08:03 +0530

    all batsman strikeRates

---
+++ strikeRates.js
@@ -0,0 +1,54 @@
+var fs = require('fs');
+var csv = require('fast-csv');
+var deliveriesStream = fs.createReadStream("deliveries.csv");
+var batsmenStrikeRate={};
+var batsmanData={};
+
+csv
+.fromStream(deliveriesStream,{headers : true})
+.on("data",function(delivery){
+    if(batsmanData.hasOwnProperty(delivery['batsman'])){
+        if(parseInt(delivery['wide_runs'])===0){
+          batsmanData[delivery['batsman']] += parseInt(delivery["batsman_runs"])  
+          batsmanData[delivery['batsman']+'balls']++;
+        }
+    }
+        else{
+            if(parseInt(delivery['wide_runs'])===0){
+                batsmanData[delivery['batsman']] = parseInt(delivery["batsman_runs"])  
+                batsmanData[delivery['batsman']+'balls'] = 1;
+              }
+    }
+
+})
+.on("end",function(){
+      batsmenStrikeRate = calculateStrikeRate(batsmanData);
+       console.log(batsmenStrikeRate);
+    })
+
+
+    function calculateStrikeRate(obj){
+                 let result={};
+               for(let batsman in obj){
+                   let strikeRate =0;
+                   if(!(/balls/).test(batsman)){
+                       strikeRate = (obj[batsman]/obj[batsman+'balls'])*100;
+                       result[batsman]=strikeRate.toFixed(2);
+                   }
+               }
+
+                return sorting(result);
+    }
+
+    function sorting(obj){
+        var arr=[]
+          for(let i in obj){
+              let ar=[];
+              ar[0]=i;
+              ar[1]=obj[i]
+            arr.push(ar); 
+        }   
+          console.log(arr.sort((a,b)=>{
+              return b[1]-a[1];
+          }))
+      }
