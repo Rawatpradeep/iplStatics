10807f993375fea5cf32280ed7f64d3919bed510
Pradeep Singh Rawat <pradeep.rawat@mountblue.io>
2018-12-07 12:03:54 +0530

    completed without most es6

--- luckiestTeam.js
+++ luckiestTeam.js
@@ -1,33 +1,55 @@
 var fs = require('fs');
 var csv = require('fast-csv');
+var luckiestTeam = {}
 var matchesStream = fs.createReadStream("matches.csv");
-var luckiestTeam={};
-
 csv
 .fromStream(matchesStream,{headers : true})
 .on("data",function(match){
-    if(luckiestTeam.hasOwnProperty(match['toss_winner'])){
-        luckiestTeam[match['toss_winner']]++;
-    }
-    else{
-        luckiestTeam[match['toss_winner']]=1;
-    }
+    getLuckestTeam(match)
 })
 .on("end",function(){
-      console.log(sorting(luckiestTeam));
-    
+          console.log(sorting(luckiestTeam));
 })
 
-function sorting(obj){
-  var output=[]
-    for(let i in obj){
-        let temp=[];
-        temp[0]=i;
-        temp[1]=obj[i]
-      output.push(temp); 
-  }   
-      output.sort((a,b)=>{
-        return b[1]-a[1];
+
+function getLuckestTeam(obj){
+        if (luckiestTeam.hasOwnProperty(obj['toss_winner'])) {
+            luckiestTeam[obj['toss_winner']]++;
+        } else {
+            luckiestTeam[obj['toss_winner']] = 1;
+        }
+    
+}
+
+function sorting(obj) {
+    var output = []
+    for (let i in obj) {
+        let temp = [];
+        temp[0] = i;
+        temp[1] = obj[i]
+        output.push(temp);
+    }
+    output.sort((a, b) => {
+        return b[1] - a[1];
     })
     return output;
+}
+
+// Chekcing the logic
+
+function getTossWon(obj) {
+    var luckiestTeam = {};
+    for(let i in obj){
+    if (obj[i]['season'] == 2017) {
+        if (luckiestTeam.hasOwnProperty(obj[i]['toss_winner'])) {
+            luckiestTeam[obj[i]['toss_winner']]++;
+        } else {
+            luckiestTeam[obj[i]['toss_winner']] = 1;
+        }
+    }}
+    luckiestTeam = sorting(luckiestTeam);
+    return luckiestTeam[0];
+}
+module.exports = {
+    getTossWon: getTossWon
 }
